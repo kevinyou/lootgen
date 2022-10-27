4a0924ca43f7e7d3cd5bb456a29e61a2ff498c90
Kevin You <kevin.you@outlook.com>
2022-10-27 00:41:47 -0500

    Add base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/coschedulerscheduler",
   plugins: [vue()],
   resolve: {
     alias: {
